e84ad2825ddd95e1bc8dc554611685d7ce30ee78
Tahir <tahirifdn@gmail.com>
2025-02-13 10:07:52 +0300

    Imu logger optimize edildi

--- vehicle_controller/vehicle_controller/imu_logger.py
+++ vehicle_controller/vehicle_controller/imu_logger.py
@@ -3,8 +3,10 @@ import rclpy
 import subprocess
 
 from rclpy.node import Node
+from threading import Lock
+from collections import deque #defaultdict ile optimize çalışması yapılacak...
 
-from nav_msgs.msg import Odometry 
+from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Imu
 
 class ImuLogger(Node):
@@ -18,57 +20,62 @@ class ImuLogger(Node):
         self.imu_file.write(f"{'time':<16} {'pose_x':<16} {'pose_y':<16} {'pose_z':<16} {'ax1':<16} {'ay1':<16} {'az1':<16} {'gx1':<16} {'gy1':<16} {'gz1':<16} {'ax2':<16} {'ay2':<16} {'az2':<16} {'gx2':<16} {'gy2':<16} {'gz2':<16} {'ax3':<16} {'ay3':<16} {'az3':<16} {'gx3':<16} {'gy3':<16} {'gz3':<16}\n")
 
         self.buffer = {}
-        self.timestamp_arr = []
+        self.buffer_lock = Lock()
+        self.timestamp_arr = deque(maxlen=1000)
 
-        self.imu1_sub = self.create_subscription(Imu, "/imu1", lambda msg: self.imu_sub_callback(msg, "imu1"), 10)
-        self.imu2_sub = self.create_subscription(Imu, "/imu2", lambda msg: self.imu_sub_callback(msg, "imu2"), 10)
-        self.imu3_sub = self.create_subscription(Imu, "/imu3", lambda msg: self.imu_sub_callback(msg, "imu3"), 10)
+        self.imu1_sub = self.create_subscription(Imu, "/imu1", lambda msg: self.imu_sub_callback(msg, "imu1"), 30)
+        self.imu2_sub = self.create_subscription(Imu, "/imu2", lambda msg: self.imu_sub_callback(msg, "imu2"), 30)
+        self.imu3_sub = self.create_subscription(Imu, "/imu3", lambda msg: self.imu_sub_callback(msg, "imu3"), 30)
 
-        self.odom_sub = self.create_subscription(Odometry, "/diff_drive_robot_controller/odom", self.odom_callback, 10)
+        self.odom_sub = self.create_subscription(Odometry, "/diff_drive_robot_controller/odom", self.odom_callback, 30)
 
     def odom_callback(self, msg):
         current_pos = msg.pose.pose.position
-        timestamp_sec = msg.header.stamp.sec
-        timestamp_nano = msg.header.stamp.nanosec
-        timestamp = f"{timestamp_sec}.{timestamp_nano}"
-                
+        timestamp = self.get_timestamp(msg)
+
         if timestamp not in self.timestamp_arr:
             self.timestamp_arr.append(timestamp)
-            self.buffer[self.timestamp_arr.index(timestamp)] = {}
-        
-        self.buffer[self.timestamp_arr.index(timestamp)]["pose"] = {
-            "pose_x": current_pos.x,
-            "pose_y": current_pos.y,
-            "pose_z": current_pos.z
-        }
+            self.buffer[timestamp] = {}
+
+
+        with self.buffer_lock:
+            self.buffer[timestamp]["pose"] = {
+                "pose_x": current_pos.x,
+                "pose_y": current_pos.y,
+                "pose_z": current_pos.z
+            }
 
     def imu_sub_callback(self, msg, imu_id):
-        timestamp_nano = msg.header.stamp.nanosec
-        timestamp_sec = msg.header.stamp.sec
-        timestamp = f"{timestamp_sec}.{timestamp_nano}"
+        timestamp = self.get_timestamp(msg)
 
         if timestamp not in self.timestamp_arr:
             self.timestamp_arr.append(timestamp)
-            self.buffer[self.timestamp_arr.index(timestamp)] = {}
+            self.buffer[timestamp] = {}
+
+        #self.get_logger().info("-----------------------------DEBUG-------------------------------")
+        #self.get_logger().info(f"Processing {imu_id} data for timestamp: {timestamp}")
+        #self.get_logger().info(str(self.buffer[str_timestamp]))
+        self.get_logger().info(timestamp)
+        #self.get_logger().info(str(len(self.timestamp_arr)))
+
+        with self.buffer_lock:
+            self.buffer[timestamp][imu_id] = {
+                "ax": msg.linear_acceleration.x,
+                "ay": msg.linear_acceleration.y,
+                "az": msg.linear_acceleration.z,
+                "gx": msg.angular_velocity.x,
+                "gy": msg.angular_velocity.y,
+                "gz": msg.angular_velocity.z,
+            }
         
-        self.buffer[self.timestamp_arr.index(timestamp)][imu_id] = {
-            "ax": msg.linear_acceleration.x,
-            "ay": msg.linear_acceleration.y,
-            "az": msg.linear_acceleration.z,
-            "gx": msg.angular_velocity.x,
-            "gy": msg.angular_velocity.y,
-            "gz": msg.angular_velocity.z,
-        }
-        
-        if len(self.buffer[self.timestamp_arr.index(timestamp)]) == 4:
+        if "pose" in self.buffer[timestamp] and all(imu in self.buffer[timestamp] for imu in ["imu1", "imu2", "imu3"]):
             self.write_to_file(timestamp)
             
-            timestamp_index = self.timestamp_arr.index(timestamp)
-            del self.buffer[timestamp_index]
-            self.timestamp_arr.remove(timestamp)
+            with self.buffer_lock:
+                del self.buffer[timestamp]
     
     def write_to_file(self, timestamp):
-        data = self.buffer[self.timestamp_arr.index(timestamp)]
+        data = self.buffer[timestamp]
 
         data_str = f"{timestamp:<15} "
         for imu_id in ["pose", "imu1", "imu2", "imu3"]:
@@ -78,11 +85,16 @@ class ImuLogger(Node):
                 data_str += f"{pose_data['pose_x']:<15.12f}, {pose_data['pose_y']:<15.12f}, {pose_data['pose_z']:<15.12f}"
             else: 
                 imu_data = data[imu_id]
-                data_str += f"{imu_data['ax']:<15.12f}, {imu_data['ay']:<15.12f}, {imu_data['az']:<15.12f}, {imu_data['gx']:<15.12f}, {imu_data['gy']:<15.12f}, {imu_data['gz']:<15.12f}, "
+                data_str += f"{imu_data['ax']:<15.12f}, {imu_data['ay']:<15.12f}, {imu_data['az']:<15.12f}, {imu_data['gx']:<15.12f}, {imu_data['gy']:<15.12f}, {imu_data['gz']:<15.12f} "
         
-        data_str = data_str.rstrip(", ") + "\n"
+        data_str = data_str.rstrip() + "\n"
         self.imu_file.write(data_str)
         self.imu_file.flush()
+    
+    def get_timestamp(self, msg):
+        timestamp_nano = msg.header.stamp.nanosec
+        timestamp_sec = msg.header.stamp.sec
+        return f"{timestamp_sec}.{timestamp_nano}"
 
 
 def main():
