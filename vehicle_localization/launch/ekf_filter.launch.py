4af7191a71c0d76caf59e7a4b7d476230b901216
Tahir <tahirifdn@gmail.com>
2025-02-26 23:06:53 +0300

    Gps verileri doğru şekilde ekf_filter'a eklendi

--- vehicle_localization/launch/ekf_filter.launch.py
+++ vehicle_localization/launch/ekf_filter.launch.py
@@ -8,6 +8,14 @@ def generate_launch_description():
 
     vehicle_localization_dir = get_package_share_directory("vehicle_localization")
 
+    # Static Transform Publisher for GPS sensor
+    gps_transform = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='gps_transform',
+        arguments=['0', '0', '0', '0', '0', '0', 'base_footprint', 'vehicle/base_footprint/gps']
+    )
+
     navsat_transform = Node(
         package="robot_localization",
         executable="navsat_transform_node",
@@ -37,6 +45,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        gps_transform,
         navsat_transform,
         ekf_node_global
     ])
